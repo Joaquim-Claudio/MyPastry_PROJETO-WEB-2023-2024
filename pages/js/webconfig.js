de996e6d13b15a127384853d33250f2ecc4efa44
Joaquim-Claudio <116195116+Joaquim-Claudio@users.noreply.github.com>
2024-04-15 02:45:29 +0100

    Started the Menu frame implementation.

--- pages/js/webconfig.js
+++ pages/js/webconfig.js
@@ -1,12 +1,37 @@
 window.onload = function() {
+
+    const btnOpenMenu = document.getElementById("ic-menu");
+    if (btnOpenMenu !== null) {
+        btnOpenMenu.addEventListener ('click', function() {
+            const menu_layout = document.getElementById("menu");
+            menu_layout.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        })
+    } 
+    
+    const btnCloseMenu = document.getElementById("close-menu");
+    if (btnCloseMenu !== null) {
+
+        btnCloseMenu.addEventListener('click', function () {
+            const menu_layout = document.getElementById("menu");
+            menu_layout.style.display = 'none';
+            document.body.style.overflow = 'visible';
+        })
+    }
     
     const ic_mouse = document.getElementById("ic-mouse");
-    ic_mouse.addEventListener('click', function() {
-       window.scroll({
-        top: 600,
-        left: 0,
-        behavior: "smooth"
-       });
-    });
+    if (ic_mouse !== null) {
+
+        ic_mouse.addEventListener('click', function() {
+           window.scroll({
+            top: 600,
+            left: 0,
+            behavior: "smooth"
+           });
+        });
+
+    }
+
+
 
 }
