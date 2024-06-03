0a4d7bed038619cd998f59a87ee9977eb73d425b
joaquim <joaquim.claudio2003@gmail.com>
2024-06-03 03:06:16 +0100

    Updated the database connection settings.

--- src/nodejs/config/db_connect.js
+++ src/nodejs/config/db_connect.js
@@ -2,6 +2,10 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize('postgres://mypastry:Rgt2cuZLknZjBzxDPacD3OCsOZOGgfEb@dpg-cpehsntds78s73f30o5g-a.frankfurt-postgres.render.com/mypastry')
+const db = new Sequelize('postgres://mypastry:Rgt2cuZLknZjBzxDPacD3OCsOZOGgfEb@dpg-cpehsntds78s73f30o5g-a.frankfurt-postgres.render.com/mypastry', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {}
+})
 
 export default db;
