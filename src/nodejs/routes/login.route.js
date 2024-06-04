663adc59f59528cccdccdef50020332ccced37ab
joaquim <joaquim.claudio2003@gmail.com>
2024-06-04 11:57:19 +0100

    Updated some login view issues.

--- src/nodejs/routes/login.route.js
+++ src/nodejs/routes/login.route.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('login');
+    res.render('login', {
+        layout: 'layouts/login'
+    });
 })
 
 export default router;
--- src/nodejs/routes/register.route.js
+++ src/nodejs/routes/register.route.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('register');
-})
+    res.render('register', {
+        layout: 'layouts/login'
+    });
+});
 
 export default router;
