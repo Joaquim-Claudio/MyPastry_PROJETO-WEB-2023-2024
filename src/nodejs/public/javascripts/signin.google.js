70a2b7e84a23ae5ca0fa48ba61fc2571e34f2655
joaquim <joaquim.claudio2003@gmail.com>
2024-06-05 10:51:06 +0100

    Updated Sign in With Google.

---
+++ src/nodejs/public/javascripts/signin.google.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+    
+    function handleCredentialResponse(response) {
+        // Enviar o token para o servidor
+        fetch('https://mypastry.onrender.com/auth/google/new', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ token: response.credential })
+        }).then(async response => {
+            if(response.ok) {
+                const data = await response.json()
+                console.log(data);
+                location.href('https://mypastry.onrender.com');
+            }
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
+    }
+})
