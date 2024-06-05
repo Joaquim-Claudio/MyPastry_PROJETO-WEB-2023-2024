3654eb5674c865e616fc172da98c907cb35eebb0
joaquim <joaquim.claudio2003@gmail.com>
2024-06-05 02:56:43 +0100

    Final tests of the Authentication resources.

--- src/nodejs/routes/register.route.js
+++ src/nodejs/routes/register.route.js
@@ -3,7 +3,7 @@ import check from '../utils/checker.js';
 
 const router = express.Router()
 
-router.get('/', check.clientSession, function(req, res) {
+router.get('/', function(req, res) {
     
     res.render('register', {
         layout: 'layouts/login'
