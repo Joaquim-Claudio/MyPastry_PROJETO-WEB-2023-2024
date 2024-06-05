e3f3eef89651323a613f70f705b92e5216e797b0
joaquim <joaquim.claudio2003@gmail.com>
2024-06-05 12:33:01 +0100

    Updated Sign in With Google.

--- src/nodejs/controllers/auth.controller.js
+++ src/nodejs/controllers/auth.controller.js
@@ -1,49 +1,13 @@
-import crypto from 'crypto';
-import {google} from 'googleapis'
-import url from 'url';
+import { OAuth2Client } from 'google-auth-library';
 import dotenv from 'dotenv';
 dotenv.config();
 
 
-import { OAuth2Client } from 'google-auth-library';
-
 import Client from '../models/client.model.js';
-
-
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GAPI_CLIENT_ID,
-    process.env.GAPI_CLIENT_SECRET,
-    process.env.GAPI_REDIRECT_URL
-  )
-  
-  const scopes = [
-    'https://www.googleapis.com/auth/drive.metadata.readonly'
-  ];
-  
-
-const Authenticate = (req, res) => {
-
-    const state = crypto.randomBytes(32).toString('hex');
-    req.session.state = state;
-
-    const authorizationUrl = oauth2Client.generateAuthUrl({
-        // 'online' (default) or 'offline' (gets refresh_token)
-        access_type: 'offline',
-        /** Pass in the scopes array defined above.
-             * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-        scope: scopes,
-        // Enable incremental authorization. Recommended as a best practice.
-        include_granted_scopes: true,
-        // Include the state parameter to reduce the risk of CSRF attacks.
-        state: state
-    });
-
-    res.redirect(authorizationUrl);
-}
 
 const authClient = new OAuth2Client()
 
-const AuthenticateNew = async (req, res) => {
+const Authenticate = async (req, res) => {
     const {token} = req.body;
     
     try {
@@ -66,6 +30,7 @@ const AuthenticateNew = async (req, res) => {
             }
 
             req.session.client = client;
+            req.session.client.isLogged = true;
             
             console.log(JSON.stringify(req.session.client, null, 2));
 
@@ -77,55 +42,8 @@ const AuthenticateNew = async (req, res) => {
 }
 
 
-const ResponseHandler = async (req, res) => {
-    const q = url.parse(req.url, true).query;
-
-    if(q.error) {
-        console.log(q.error);
-
-    } else if (q.state !== req.session.state) {
-        console.log('State mismatch. Possible CSRF attack');
-        res.end('State mismatch. Possible CSRF attack');
-
-
-    } else{
-        const {tokens} = await oauth2Client.getToken(q.code);
-        oauth2Client.setCredentials(tokens);
-
-        try {
-            const ticket = await oauth2Client.verifyIdToken({
-              idToken: tokens.id_token,
-              audience: process.env.GAPI_CLIENT_ID,
-            });
-
-            const userData = ticket.getPayload();
-            const google_id = userData['sub'];
-
-            let client = await Client.findOne({where: {google_id}});
-
-            if(client === null) {
-                client = await Client.create({
-                    name: userData.name,
-                    email: userData.email,
-                    google_id: google_id
-                })
-            }
-
-            console.log(JSON.stringify(client, null, 2));
-
-            req.session.client = client;
-
-            const url = 'https://mypastry.onrender.com/';
-            res.redirect('/auth/redirect');
-
-        } catch (error) {
-            res.status(401).json('Failed to sign in.');
-        }
-    }
-}
-
 const Redirect = (req, res) => {
     return res.status(200).send();
 }
 
-export {Authenticate, ResponseHandler, AuthenticateNew, Redirect};
+export {Authenticate, Redirect};
--- src/nodejs/routes/auth.route.js
+++ src/nodejs/routes/auth.route.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import check from '../utils/checker.js';
-import { Authenticate, ResponseHandler, AuthenticateNew, Redirect } from '../controllers/auth.controller.js';
+import { Authenticate, Redirect } from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
 router.get('/google', check.clientSession,  Authenticate);
 router.get('/redirect', Redirect);
-
-router.post('/google/new', check.clientSession, AuthenticateNew)
 
 
 export default router;
